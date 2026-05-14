6da63454361b01764f2edb29fc45d2989b335351
Prince Ugochukwu <princeugo814@gmail.com>
2026-05-14 16:23:16 +0100

    adding up

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tailwindcss';
+
+export default defineConfig({
+  theme: {
+    extend: {
+      colors: {
+        primary: '#14b8a6',
+        'brand-teal': '#14b8a6',
+        'brand-tealLight': '#2dd4bf',
+      },
+    },
+  },
+});
